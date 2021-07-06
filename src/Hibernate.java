cc4ae5cf603a786635a5c932a9b983ae1dad6f56
maria-siddiqui <mariesid04@gmail.com>
2021-07-06 17:14:14 +0500

    first commit

---
+++ src/Hibernate.java
@@ -0,0 +1,30 @@
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.internal.SessionFactoryServiceRegistryBuilderImpl;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+
+public class Hibernate {
+	public static void main(String[] args){
+		
+		Job person = new Job();
+		
+		person.setId(50); 
+		person.setFname("Huma");
+		person.setLname("Karim");
+		person.setDepartment("Admin");
+		person.setSalary(45000);
+		
+		Configuration config = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Job.class);
+//		ServiceRegistry reg = new SessionFactoryServiceRegistryBuilderImpl()
+//				.applySettings(config.getProperties()).buildServiceRegistry();
+		SessionFactory sf = config.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+		session.save(person);
+		tx.commit();
+		
+	}
+
+}
---
+++ src/Job.java
@@ -0,0 +1,53 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.Column;
+
+@Entity
+@Table(name="People")
+public class Job //Job bean or class
+{
+	@Id
+	private int id;
+	@Column
+	private String fname;
+	@Transient
+	private String lname;
+	@Column(name="departName")
+	private String department;
+	private double salary;
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	public void setFname(String fname){
+		this.fname=fname;
+	}
+	public void setLname(String lname){
+		this.lname=lname;
+	}
+	public void setDepartment(String department){
+		this.department=department;
+	}
+	public void setSalary(double salary){
+		this.salary=salary;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	public String getFname(){
+		return fname;
+	}
+	public String getLname(){
+		return lname;
+	}
+	public String getDepartment(){
+		return department;
+	}
+	public double getSalary(){
+		return salary;
+	}
+	
+}
